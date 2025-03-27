f36efc35fe723d539108a9bdafd8cfbafaba9604
Pedro Renan <pedroviana15094@gmail.com>
2025-03-27 18:10:28 -0300

    fix: function name in getProducts

--- src/handlers/getProducts.ts
+++ src/handlers/getProducts.ts
@@ -3,23 +3,22 @@ import { getProducts } from "../services/dynamoService";
 
 
 export const handler: APIGatewayProxyHandler = async () => {
-  try{
+  try {
     const products = await getProducts();
 
     return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "Produtos Encontrados",
-          data: products
-        }),
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Produtos Encontrados",
+        data: products
+      }),
     };
 
-  }catch(error){
+  } catch (error) {
     console.log("Erro ao buscar produtos", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Erro interno do servidor"}),
+      body: JSON.stringify({ message: "Erro interno do servidor" }),
     };
   }
-
 };
