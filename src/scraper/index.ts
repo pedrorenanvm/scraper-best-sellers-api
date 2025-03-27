41552bd34c3e925357e3ed41281e2bbd728f9c28
Pedro Renan <pedroviana15094@gmail.com>
2025-03-27 11:22:16 -0300

    feat:add local product scrapper

---
+++ src/scraper/index.ts
@@ -0,0 +1,35 @@
+import puppeteer from "puppeteer";
+
+
+export async function getBestSellers() {
+  const url = "https://www.amazon.com.br/bestsellers/";
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36"
+  );
+
+
+  await page.goto(url, { waitUntil: "networkidle2", timeout: 0});
+
+  await page.waitForSelector(".p13n-sc-truncate-desktop-type2", { timeout: 5000 }); 
+
+  const products = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".p13n-sc-truncate-desktop-type2"))
+      .slice(0,3)
+      .map((el) => ({
+        name : el.textContent?.trim() || "Nome não encontrado",
+      }));
+  });
+
+  await browser.close();
+  return products;
+
+}
+  
+if(require.main === module) {
+    getBestSellers()
+      .then(console.log)
+      .catch(console.error);
+}
